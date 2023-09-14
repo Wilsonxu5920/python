fa772e089c0b91c2c2a756e0195ad6e59aed5397
WXu <2844355984@qq.com>
2023-09-13 19:46:00 -0700

    https://github.com/Wilsonxu5920/python.git

--- Chapter 2/question 15.4.py
+++ Chapter 2/question 15.4.py
@@ -1,15 +1,15 @@
 import turtle
-t = turtle.Turtle()
-t.forward(50)
-t.left(105)
-t.forward(130.6506)
-t.left(150)
-t.forward(130.6506)
-t.left(150)
-t.forward(50)
-t.right=(60)
-t.forward(50)
+turtle. showturtle()
+turtle. left(60)
+turtle. forward(100)
+turtle. right(120)
+turtle. forward(100)
+turtle. right(150)
+turtle. forward(57.735)
+turtle. left(60)
+turtle. forward(57.735)
+turtle. left(150)
+turtle. forward(100)
 tuple.done
-
 
 
