917971c981373a45b47f4162cda6edaab1a24aa3
WXu <2844355984@qq.com>
2023-09-15 14:55:21 -0700

    22

--- chapter 4/question2.py
+++ chapter 4/question2.py
@@ -1,6 +1,15 @@
 calories_minute = 4.2
-minutes = 5
-for minutes in range (4):
-    minutes = minutes+5
-    calories_burned = minutes * calories_minute
-print(calories_burned)
+
+
+calories_burned_1 = 10 * calories_minute
+calories_burned_2 = 15 * calories_minute
+calories_burned_3 = 20 * calories_minute
+calories_burned_4 = 25 * calories_minute
+calories_burned_5 = 30 * calories_minute
+print("calories burned after 10:",calories_burned_1)
+print("calories burned after 15:",calories_burned_2)
+print("calories burned after 20:",calories_burned_3)
+print("calories burned after 25:",calories_burned_4)
+print("calories burned after 30:",calories_burned_5)
+
+
