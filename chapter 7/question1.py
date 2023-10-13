f891dffe78d68654181529c417c5eb12b1d0295f
WXu <2844355984@qq.com>
2023-10-12 23:08:18 -0700

    1314

---
+++ chapter 7/question1.py
@@ -0,0 +1,3 @@
+number_list = [74, 1, 9, 105, 20, -2, 67, 77, 124, -45, 38]
+average_of_the_values = sum(number_list) / len(number_list)
+print("The average of the values is:", average_of_the_values)
