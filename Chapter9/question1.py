f7516de30828be5f0c4a50cbc22151f10453c722
Wilsonxu5920 <143638983+Wilsonxu5920@users.noreply.github.com>
2023-11-08 00:34:54 -0800

    Update question1.py

--- Chapter9/question1.py
+++ Chapter9/question1.py
@@ -1,3 +1,5 @@
+# Dictionaries containing data about the Galilean moons of Jupiter.
+# Each moon has its mean radius in kilometers, surface gravity in m/s², and orbital period in days.
 mean_radius_dict = {
     "Io": 1821.6,
     "Europa": 1560.8,
@@ -18,7 +20,9 @@ orbital_period_dict = {
     "Ganymede": 7.154,
     "Callisto": 16.689
 }
-
+# Function to display moon data.
+# If the moon is in the dictionary, it prints its mean radius, surface gravity, and orbital period.
+# If the moon is not found, it prints an error message.
 def display_moon_data(moon_name):
     if moon_name in mean_radius_dict:
         print(f"Mean Radius: {mean_radius_dict[moon_name]} kilometers")
@@ -26,7 +30,8 @@ def display_moon_data(moon_name):
         print(f"Orbital Period: {orbital_period_dict[moon_name]} days")
     else:
         print("Moon not found.")
-
+# Main loop that continues to ask the user to enter a moon name.
+# It breaks and exits if the user types 'quit'.
 
 while True:
     moon_name = input("Enter the name of a Galilean moon of Jupiter (Io, Europa, Ganymede, Callisto) or 'quit' to exit: ").capitalize()
