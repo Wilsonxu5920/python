6ec4267ab621e25429ae6382a86805955cb2ff02
Wilsonxu5920 <143638983+Wilsonxu5920@users.noreply.github.com>
2023-11-08 00:19:26 -0800

    Update question4.py

--- Chapter7/question4.py
+++ Chapter7/question4.py
@@ -1,16 +1,23 @@
 numbers = []
 
+# Loop 20 times to collect 20 numbers
 for i in range(20):
     while True:
+        # Prompt the user to input a number and cast it to float
         number = float(input(f"Enter number {i + 1}: "))
+        # Append the number to the list of numbers
         numbers.append(number)
+        # Break out of the inner while loop
         break
 
+# Calculate the lowest 
 lowest = min(numbers)
+# Calculate the highest number 
 highest = max(numbers)
 
-
+# Calculate the total sum of the numbers 
 total = sum(numbers)
+# Calculate the average of the numbers 
 average = total / len(numbers)
 
 print(f"Lowest number: {lowest}")
